0373749b9d7a1aa6499d543cbd90dadeadb79bed
Neale Johnston <neale@team-johnston.com>
2020-12-07 11:42:34 +0000

    one user only

--- db/seeds.js
+++ db/seeds.js
@@ -1,52 +1,7 @@
 use stocks;
 db.dropDatabase();
 
-db.neale.insertMany([
-  {
-    username: 'Neale'
-  },
-  {
-    symbol: 'AAPL',
-    name: 'APPLE',
-    quantity: 1500,
-    date: '2020-12-06',
-    purchasedPrice: 100,
-    currentPrice: 125,
-  },
-  {
-    symbol: 'TSLA',
-    name: 'TESLA',
-    quantity: 2000,
-    date: '2020-03-04',
-    purchasedPrice: 500,
-    currentPrice: 700,
-  },
-])
-db.ian.insertMany([
-  {
-    username: 'Ian'
-  },
-  {
-    symbol: 'AAPL',
-    name: 'APPLE',
-    quantity: 1500,
-    date: '2020-12-06',
-    purchasedPrice: 100,
-    currentPrice: 125,
-  },
-  {
-    symbol: 'TSLA',
-    name: 'TESLA',
-    quantity: 2000,
-    date: '2020-03-04',
-    purchasedPrice: 500,
-    currentPrice: 700,
-  },
-])
-db.michael.insertMany([
-  {
-    username: 'Michael'
-  },
+db.stock.insertMany([
   {
     symbol: 'AAPL',
     name: 'APPLE',
