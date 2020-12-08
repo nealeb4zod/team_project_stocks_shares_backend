6a9be83975f1c489575feca976bbd14cfc98abec
Neale Johnston <neale@team-johnston.com>
2020-12-08 13:31:19 +0000

    Different stocks for differnet fols

--- db/seeds.js
+++ db/seeds.js
@@ -25,18 +25,18 @@ db.stocks.insertMany([
     userName: "Michael",
     stocks: [
       {
-        symbol: 'AAPL',
-        name: 'APPLE',
-        quantity: 1500,
-        date: '2020-12-06',
-        purchasedPrice: 100,
+        symbol: 'AACQW',
+        name: 'Artius Acquisition Inc - Warrants (13/07/2025)',
+        quantity: 150,
+        date: '2018-10-01',
+        purchasedPrice: 20,
       },
       {
-        symbol: 'TSLA',
-        name: 'TESLA',
+        symbol: 'TRI',
+        name: 'Thomson-Reuters Corp',
         quantity: 2000,
-        date: '2020-03-04',
-        purchasedPrice: 500,
+        date: '2016-03-04',
+        purchasedPrice: 45,
       },
     ]
   },
@@ -44,17 +44,17 @@ db.stocks.insertMany([
     userName: "Ian",
     stocks: [
       {
-        symbol: 'AAPL',
-        name: 'APPLE',
-        quantity: 1500,
-        date: '2020-12-06',
-        purchasedPrice: 100,
+        symbol: 'MSFT',
+        name: 'Microsoft Corporation',
+        quantity: 3000,
+        date: '20150-12-06',
+        purchasedPrice: 125,
       },
       {
         symbol: 'TSLA',
         name: 'TESLA',
-        quantity: 2000,
-        date: '2020-03-04',
+        quantity: 4000,
+        date: '2019-03-04',
         purchasedPrice: 500,
       },
     ]
