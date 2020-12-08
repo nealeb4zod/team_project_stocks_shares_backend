de9106fba1a199fc17fbde1546c50346a58314f6
Neale Johnston <neale@team-johnston.com>
2020-12-08 13:25:16 +0000

    can now add stocks to individual user

--- db/seeds.js
+++ db/seeds.js
@@ -3,17 +3,60 @@ db.dropDatabase();
 
 db.stocks.insertMany([
   {
-    symbol: 'AAPL',
-    name: 'APPLE',
-    quantity: 1500,
-    date: '2020-12-06',
-    purchasedPrice: 100,
+    userName: "Neale",
+    stocks: [
+      {
+        symbol: 'AAPL',
+        name: 'APPLE',
+        quantity: 1500,
+        date: '2020-12-06',
+        purchasedPrice: 100,
+      },
+      {
+        symbol: 'TSLA',
+        name: 'TESLA',
+        quantity: 2000,
+        date: '2020-03-04',
+        purchasedPrice: 500,
+      },
+    ]
   },
   {
-    symbol: 'TSLA',
-    name: 'TESLA',
-    quantity: 2000,
-    date: '2020-03-04',
-    purchasedPrice: 500,
+    userName: "Michael",
+    stocks: [
+      {
+        symbol: 'AAPL',
+        name: 'APPLE',
+        quantity: 1500,
+        date: '2020-12-06',
+        purchasedPrice: 100,
+      },
+      {
+        symbol: 'TSLA',
+        name: 'TESLA',
+        quantity: 2000,
+        date: '2020-03-04',
+        purchasedPrice: 500,
+      },
+    ]
   },
+  {
+    userName: "Ian",
+    stocks: [
+      {
+        symbol: 'AAPL',
+        name: 'APPLE',
+        quantity: 1500,
+        date: '2020-12-06',
+        purchasedPrice: 100,
+      },
+      {
+        symbol: 'TSLA',
+        name: 'TESLA',
+        quantity: 2000,
+        date: '2020-03-04',
+        purchasedPrice: 500,
+      },
+    ]
+  }
 ])
